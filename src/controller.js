90907581570c723bfe4efa9f9d28beb2bedb9f82
Gerson Vásquez <gdvr1991@gmail.com>
2021-03-13 16:53:30 -0400

    probando conexion con soap

--- src/controller.js
+++ src/controller.js
@@ -1,10 +1,10 @@
 import valid from "./helpers";
 import soapService from "./services";
 
-export default async (req, res, operation, status) => {
-  const errors = valid(req.body, operation);
-  if (errors.length > 0) return res.status(422).json({ error: false, errors });
-  const response = await soapService(operation, req.body);
+export default async (body, res, operation, status) => {
+  const errors = valid(body, operation);
+  if (errors.length > 0) return res.status(422).json({ error: true, errors });
+  const response = await soapService(operation, body);
   if (response.error) return res.status(422).json(response);
   return res.status(status).json(response);
 };
--- src/services.js
+++ src/services.js
@@ -1,2 +1,23 @@
-/* eslint-disable */
-export const soapService = (operation, data) => {};
+const soap = require("soap");
+
+// const url = "http://127.0.0.1:1337/wallet?wsdl";
+const url = "http://127.0.0.1:1337/testService?wsdl";
+
+const soapService = (operation, data) => {
+  console.log("saliendo al soap", url);
+  // const args = { operation, data };
+  return new Promise((resolve) => {
+    soap.createClientAsync(url, (err, client) => {
+      if (err) console.log("error del client", err);
+      else {
+        client.test1({ myArg1: "hola", myArg2: "mundo" }, (err2, result) => {
+          if (err) console.log("error del soap", err2.toString());
+          console.log("llego del soap", result);
+          resolve(result);
+        });
+      }
+    });
+  });
+};
+
+export default soapService;
