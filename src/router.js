8757f0667857e1357e1c4942ab2abced6868370f
Gerson Vásquez <gdvr1991@gmail.com>
2021-03-15 04:44:53 -0400

    rest api listo

--- src/router.js
+++ src/router.js
@@ -11,13 +11,18 @@ router.post("/api/recharge", (req, res) =>
   controller(req.body, res, "RechargeWallet", 200)
 );
 
-router.post("/api/payment", (req, res) =>
-  controller(req.body, res, "PayWithWallet", 200)
-);
+router.post("/api/payment", (req, res) => {
+  res.cookie("auth-wallet", Math.round(Math.random() * 999999), {
+    expire: new Date().setMinutes(new Date().getMinutes() + 10),
+  });
+  req.body.session = req.cookies["auth-wallet"];
+  controller(req.body, res, "PayWithWallet", 200);
+});
 
-router.get("/api/confirm-token/:token", (req, res) =>
-  controller(req.params, res, "ConfirmToken", 200)
-);
+router.get("/api/confirm-token/:token", (req, res) => {
+  req.params.session = req.cookies["auth-wallet"];
+  controller(req.params, res, "ConfirmToken", 200);
+});
 
 router.post("/api/balance", (req, res) =>
   controller(req.body, res, "GetBalance", 200)
--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { config } from "dotenv";
 import router from "./router";
 
 const app = express();
+config();
+
+const PORT = process.env.PORT || 9000;
+const HOST = process.env.HOST || "127.0.0.1";
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static("public"));
 
 app.use(router);
 
-app.listen(9000, () => console.info("Server is running"));
+app.listen(PORT, HOST, () => console.info("Server is running"));
